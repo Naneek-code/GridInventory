3e027d589bb93eeda49b474888bc9bd141cc3e33
montesi <keenandigimon@gmail.com>
2026-08-08 02:38:47 -0300

    remove unused files

--- Contents/mods/GridInventory/42.20/media/lua/client/CheckPants.lua
+++
@@ -1,7 +0,0 @@
-local function checkPants()
-    local item = ScriptManager.instance:getItem("Base.Trousers")
-    if item then
-        print("TROUSERS BODY LOC: " .. tostring(item:getBodyLocation()))
-    end
-end
-Events.OnGameBoot.Add(checkPants)
